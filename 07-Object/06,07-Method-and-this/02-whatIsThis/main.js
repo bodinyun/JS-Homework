3919cdb98b91de8c5aabe1cceb3fd3bc45c1d2f5
Yun <109775643+bodinyun@users.noreply.github.com>
2022-08-08 08:52:32 +0700

    Delete 07-Object directory

--- 07-Object/06,07-Method-and-this/02-whatIsThis/main.js
+++
@@ -1,9 +0,0 @@
-var name = 'Joe';
-
-function makeUser() { 
-  const obj = {name: 'John',ref: this};
-  return obj
-}
-let user = makeUser();
-console.log(user)
-console.log(user.ref.name); // user.ref == this == window  then this.name == 'Joe' *
